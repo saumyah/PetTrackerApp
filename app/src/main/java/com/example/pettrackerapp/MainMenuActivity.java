e584ad7c1f627864480676754dfac3d48609f4e6
Saumyah Patel <saumyah@hotmail.com>
2021-02-26 16:32:14 -0500

    Continued Updating Project

---
+++ app/src/main/java/com/example/pettrackerapp/MainMenuActivity.java
@@ -0,0 +1,33 @@
+package com.example.pettrackerapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MainMenuActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView((R.layout.fragment_game_options));
+    }
+
+    public void onResume() {
+        super.onResume();
+    }
+    public void onStart() {
+        super.onStart();
+    }
+    public void onStop() {
+        super.onStop();
+    }
+    public void onPause() {
+        super.onPause();
+    }
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+}
