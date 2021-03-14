540eb7affad41239e8a2163912e361ae7fd8de2b
akhan31 <khan.625@osu.edu>
2021-03-14 17:56:19 -0400

    CRUD operations

--- app/src/main/java/com/example/pettrackerapp/DatabaseStorage.java
+++ app/src/main/java/com/example/pettrackerapp/DatabaseStorage.java
@@ -9,39 +9,61 @@ import timber.log.Timber;
 
 public class DatabaseStorage extends SQLiteOpenHelper {
      static final int DATABASE_VERSION = 1;
-     static final String DATABASE_NAME = "Pets";
+     static final String DATABASE_NAME = "PetsDatabase";
      //public  SQLiteDatabase db;
 
      public DatabaseStorage(Context context) {
          super(context, DATABASE_NAME, null, DATABASE_VERSION);
      }
      //create table
-     @override
+     @Override
      public void onCreate(SQLiteDatabase db){
-         db.execSQL("Create Table " + "Pets" + "(" + "ID " + "Integer Primary Key," + "Name "
-                 + "Text," + "Type " + "Text" + ")");
+         db.execSQL("Create Table " + "PETS" + "(" + "ID " + "Integer Primary Key," + "NAME "
+                 + "Text," + "TYPE " + "Text" + ")");
      }
      //upgrade table by dropping old table and creating new one
-     @override
+     @Override
      public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-         db.execSQL("DROP TABLE IF EXISTS " + "Pets");
+         db.execSQL("DROP TABLE IF EXISTS " + "PETS");
          onCreate(db);
      }
      // CRUD Operations
     //creation
-    public void insertPet(){
-
+    public void createPet(String name, String type){
+         SQLiteDatabase db = this.getWritableDatabase();
+         ContentValues newVals = new ContentValues();
+         newVals.put("NAME", name);
+         newVals.put("TYPE", type);
+         db.insert("PETS", null, newVals);
+         db.close();
     }
     //retrieval
     public String retrieveEntry(int id){
-
+        SQLiteDatabase db = this.getWritableDatabase();
+         Cursor cursor = db.query("PETS", null, "ID=?", new String[]{String.valueOf(id)}, null, null, null);
+         if (cursor == null){
+             cursor.close();
+             return "Pet does not exist";
+         }
+         cursor.moveToFirst();
+         String name = cursor.getString(cursor.getColumnIndex("NAME"));
+         cursor.close();
+         db.close();
+         return name;
     }
     //update
-    public void updateEntry(){
-
+    public void updateEntry(String name, String type, int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues newVals = new ContentValues();
+        newVals.put("NAME", name);
+        newVals.put("TYPE", type);
+        db.update("PETS", newVals, "ID = ?", new String[] {String.valueOf(id)});
+        db.close();
     }
     //deletion
-    public void deleteEntry(){
-
+    public void deleteEntry(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("PETS", "ID = ?", new String[] {String.valueOf(id)});
+        db.close();
     }
 }
