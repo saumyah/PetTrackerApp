17bcabe28a8e78631f2f00de182b2bcb6b5cc639
Saumyah Patel <saumyah@hotmail.com>
2021-03-29 13:38:27 -0400

    Continued Updating Project

--- app/src/main/java/com/example/pettrackerapp/AlarmActivity.java
+++ app/src/main/java/com/example/pettrackerapp/AlarmActivity.java
@@ -2,17 +2,50 @@ package com.example.pettrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.util.Log;
+import android.widget.Toast;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.widget.Toast;
 
 public class AlarmActivity extends AppCompatActivity {
 
     private static final String TAG = "AlarmActivity";
+    private AlarmManager am;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        setAlarm(context);
+    }
+
+    public void setAlarm(Context context)
+    {
+        AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, AlarmActivity.class);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 10, pi); // Millisec * Second * Minute
+    }
+
+    public void cancelAlarm(Context context)
+    {
+        Intent intent = new Intent(context, AlarmActivity.class);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.cancel(sender);
     }
 
     public void onStart() {
