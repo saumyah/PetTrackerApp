3376789f87c52b0ca33807ee3310cdc466e0a74a
Saumyah Patel <saumyah@hotmail.com>
2021-04-05 21:56:55 -0400

    Continued Updating Project

--- app/src/main/java/com/example/pettrackerapp/AlarmActivity.java
+++ app/src/main/java/com/example/pettrackerapp/AlarmActivity.java
@@ -1,6 +1,7 @@
 package com.example.pettrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -13,74 +14,94 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class AlarmActivity extends AppCompatActivity implements View.OnClickListener{
+public class AlarmActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     private static final String TAG = "AlarmActivity";
     private AlarmManager am;
     private Context context;
-    private TimePickerDialog timePicker;
-    Calendar calendar;
-    int currentHour;
-    int currentMinute;
-    Button setTime;
-    Button setAlarm;
-    EditText hour;
-    EditText min;
+    private Calendar calendar;
+    private int currentHour;
+    private int currentMinute;
+    private Button back;
+    private Button cancel;
+    private Button setAlarm;
+    private TextView mAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-        setTime = findViewById(R.id.buttonTime);
-        setTime.setOnClickListener(this);
+
+        mAlarm = findViewById(R.id.textViewAlarm);
+
+        back = findViewById(R.id.buttonBack2);
+        back.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        cancel = findViewById(R.id.buttonCancel);
+        cancel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                cancelAlarm();
+            }
+        });
+
         setAlarm = findViewById(R.id.buttonAlarm2);
-        setTime.setOnClickListener(this);
+        setAlarm.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
     }
 
     @Override
-    public void onClick(View v) {
-        final int viewId = v.getId();
-        if(viewId == R.id.buttonBack2){
-            finish();
-        }
-        if(viewId == R.id.buttonTime){
-            calendar = Calendar.getInstance();
-            currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-            currentMinute = calendar.get(Calendar.MINUTE);
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+        calendar.set(Calendar.MINUTE,minute);
+        calendar.set(Calendar.SECOND,0);
 
-            timePicker = new TimePickerDialog(AlarmActivity.this, new TimePickerDialog.OnTimeSetListener() {
-                @Override
-                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                    hour.setText(String.format("%02d", hourOfDay));
-                    min.setText(String.format("%02d", minute));
-                }
-            }, currentHour, currentMinute, false);
+        updateTimeTest(calendar);
+        startAlarm(calendar);
+    }
 
-            timePicker.show();
-        }
-        if(viewId == R.id.buttonAlarm2){
-            if(!hour.getText().toString().isEmpty() && !min.getText().toString().isEmpty()) {
-                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-                intent.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(hour.getText().toString()));
-                intent.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(min.getText().toString()));
-                intent.putExtra(AlarmClock.EXTRA_MESSAGE, "Time to feed your pet");
-                if(intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(AlarmActivity.this, "No Supporting Apps", Toast.LENGTH_SHORT).show();
-                }
-            }
-            else{
-                Toast.makeText(AlarmActivity.this, "Please choose a time", Toast.LENGTH_SHORT).show();
-            }
+    private void updateTimeTest(Calendar c){
+        String timeText = "Alarm set for: ";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
 
-        }
+        mAlarm.setText(timeText);
+    }
 
+    private void startAlarm(Calendar c){
+        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1,intent,0);
+        am.setExact(AlarmManager.RTC_WAKEUP,c.getTimeInMillis(),pendingIntent);
+    }
+
+    private void cancelAlarm(){
+        am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1,intent,0);
+        am.cancel(pendingIntent);
+        mAlarm.setText("Alarm Cancelled");
     }
 
 
